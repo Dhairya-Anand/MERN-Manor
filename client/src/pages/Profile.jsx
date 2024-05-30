a3c0a2b54af514e22e075d1fa39057d27c7c9a40
Dhairya Anand <dhairyaanand200211@gmail.com>
2024-05-31 03:31:04 +0530

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,7 +13,10 @@ import {
   updateUserFailure,
   deleteUserStart,
   deleteUserSuccess,
-  deleteUserFailure
+  deleteUserFailure,
+  signOutUserStart,
+  signInSuccess,
+  signInFailure
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -101,6 +104,22 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async() =>{
+    try{
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    }
+    catch(error){
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -164,7 +183,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700">{error ? error : ""}</p>
       <p className="text-green-700">
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth.controllers");
 router.post("/signup",authController.signup);
 router.post("/signin",authController.signin);
 router.post("/google",authController.google);
+router.get("/signout",authController.signout);
 
 module.exports = router;
