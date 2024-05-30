7072334e6554c54d369bd6b4a6977268be583e45
Dhairya Anand <dhairyaanand200211@gmail.com>
2024-05-31 03:18:56 +0530

    add delete user functionality

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -2,7 +2,7 @@ const bcryptjs = require("bcryptjs");
 const User = require("../models/user.model");
 const errorHandler = require("../utils/error");
 
-const updateUser = async (req, res,next) => {
+const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, "Unauthorized"));
   try {
@@ -31,6 +31,20 @@ const updateUser = async (req, res,next) => {
   }
 };
 
+const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted");
+  }
+  catch(error){
+    next(error);
+  }
+};
+
 module.exports = {
   updateUser,
+  deleteUser,
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,5 +3,6 @@ const UserController = require("../controllers/user.controllers");
 const verifyUser = require("../utils/verifyUser");
 
 router.post("/update/:id", verifyUser.verifyToken,UserController.updateUser);
+router.delete("/delete/:id", verifyUser.verifyToken,UserController.deleteUser);
 
 module.exports = router;
