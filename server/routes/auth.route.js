8752d079c0fa88f4d8a6afa48c17e9ae692e9790
Dhairya Anand <dhairyaanand200211@gmail.com>
2024-06-05 04:18:46 +0530

    bug resolved

--- server/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authController = require("../controllers/auth.controllers");
-
-router.post("/signup",authController.signup);
-router.post("/signin",authController.signin);
-router.post("/google",authController.google);
-router.get("/signout",authController.signout);
-
-module.exports = router;
--- server/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const UserController = require("../controllers/user.controllers");
-const verifyUser = require("../utils/verifyUser");
-
-router.post("/update/:id", verifyUser.verifyToken,UserController.updateUser);
-router.delete("/delete/:id", verifyUser.verifyToken,UserController.deleteUser);
-
-module.exports = router;
